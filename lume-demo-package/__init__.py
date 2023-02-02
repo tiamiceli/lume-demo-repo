777554b04b46215aea190000be4a97ebee54a511
Tia Miceli <tiamiceli@gmail.com>
2023-02-02 15:34:18 -0600

    Install versioneer.

--- lume-demo-package/__init__.py
+++ lume-demo-package/__init__.py
@@ -3,3 +3,6 @@ from lume_model.utils import variables_from_yaml
 
 with open(VARIABLE_FILE, "r") as f:
     INPUT_VARIABLES, OUTPUT_VARIABLES = variables_from_yaml(f)
+
+from . import _version
+__version__ = _version.get_versions()['version']
